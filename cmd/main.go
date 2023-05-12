d9f03109b06288d0c87360f8d016131806ffceb2
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-05-12 17:42:19 -0400

    load background image

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,31 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var (
+	err        error
+	background *ebiten.Image
+)
+
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space-background.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	opt := &ebiten.DrawImageOptions{}
+	opt.GeoM.Translate(0, 0)
+	screen.DrawImage(background, opt)
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "Hello, World!"); err != nil {
 		log.Fatal(err)
 	}
 }
