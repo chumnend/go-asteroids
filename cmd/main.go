f6a22268028221e5f363124e3c2e6771c019102e
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-05-12 17:54:15 -0400

    render player sprite

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,40 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+const (
+	SCREEN_HEIGHT = 480
+	SCREEN_WIDTH  = 640
+)
+
 var (
 	err        error
 	background *ebiten.Image
+	spaceShip  *ebiten.Image
+	player     Player
 )
+
+type Player struct {
+	image      *ebiten.Image
+	xPos, yPos float64
+	speed      float64
+}
 
 func init() {
 	background, _, err = ebitenutil.NewImageFromFile("assets/space-background.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	spaceShip, _, err = ebitenutil.NewImageFromFile("assets/spaceship.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	player = Player{
+		image: spaceShip,
+		xPos:  SCREEN_WIDTH / 2,
+		yPos:  SCREEN_HEIGHT / 2,
+		speed: 4,
 	}
 }
 
@@ -27,11 +52,15 @@ func update(screen *ebiten.Image) error {
 	opt.GeoM.Translate(0, 0)
 	screen.DrawImage(background, opt)
 
+	playerOpt := &ebiten.DrawImageOptions{}
+	playerOpt.GeoM.Translate(player.xPos, player.yPos)
+	screen.DrawImage(player.image, playerOpt)
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 640, 480, 1, "Hello, World!"); err != nil {
+	if err := ebiten.Run(update, SCREEN_WIDTH, SCREEN_HEIGHT, 1, "Hello, World!"); err != nil {
 		log.Fatal(err)
 	}
 }
