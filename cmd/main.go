edf3d77a87728f1b2f5ef783d92a0359dc1a328c
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-05-15 03:24:44 -0400

    fit code to match ebiten v2

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"gophy-runner/internal/engine"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
@@ -12,71 +12,11 @@ const (
 	SCREEN_WIDTH  = 640
 )
 
-var (
-	err        error
-	background *ebiten.Image
-	spaceShip  *ebiten.Image
-	player     Player
-)
-
-type Player struct {
-	image      *ebiten.Image
-	xPos, yPos float64
-	speed      float64
-}
-
-func init() {
-	background, _, err = ebitenutil.NewImageFromFile("assets/space-background.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	spaceShip, _, err = ebitenutil.NewImageFromFile("assets/spaceship.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	player = Player{
-		image: spaceShip,
-		xPos:  SCREEN_WIDTH / 2,
-		yPos:  SCREEN_HEIGHT / 2,
-		speed: 4,
-	}
-}
-
-func movePlayer() {
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		player.yPos -= player.speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		player.yPos += player.speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		player.xPos -= player.speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		player.xPos += player.speed
-	}
-}
-
-func update(screen *ebiten.Image) error {
-	movePlayer()
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-	opt := &ebiten.DrawImageOptions{}
-	opt.GeoM.Translate(0, 0)
-	screen.DrawImage(background, opt)
-
-	playerOpt := &ebiten.DrawImageOptions{}
-	playerOpt.GeoM.Translate(player.xPos, player.yPos)
-	screen.DrawImage(player.image, playerOpt)
-
-	return nil
-}
-
 func main() {
-	if err := ebiten.Run(update, SCREEN_WIDTH, SCREEN_HEIGHT, 1, "Hello, World!"); err != nil {
+	game := &engine.Game{}
+	ebiten.SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+	ebiten.SetWindowTitle("GOPHY RUNNER")
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
