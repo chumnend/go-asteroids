ff59bba8ff21ccc10c34f3afa4a519c783f2a952
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-05-15 03:27:33 -0400

    remove spaceship assets and code

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-	"gophy-runner/internal/engine"
-	"log"
-
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
-	SCREEN_HEIGHT = 480
-	SCREEN_WIDTH  = 640
+	SCREEN_WIDTH  = 300
+	SCREEN_HEIGHT = 300
 )
 
+type Game struct{}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return SCREEN_WIDTH, SCREEN_HEIGHT
+}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "This is a test.")
+}
+
 func main() {
-	game := &engine.Game{}
-	ebiten.SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT)
-	ebiten.SetWindowTitle("GOPHY RUNNER")
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
+	ebiten.SetWindowSize(SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
+	ebiten.SetWindowTitle("Ebiten Test")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		panic(err)
 	}
 }
