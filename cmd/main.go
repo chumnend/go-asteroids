60ef6e9e07832a1950f5dba15c2d15b7046e8a19
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-05-15 12:58:34 -0400

    load background into game

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,82 @@
 package main
 
 import (
+	"log"
+	"strconv"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
-	SCREEN_WIDTH  = 300
-	SCREEN_HEIGHT = 300
+	SCREEN_WIDTH  = 320
+	SCREEN_HEIGHT = 256
+
+	SCALE = 2
+
+	WINDOW_WIDTH  = SCREEN_WIDTH * SCALE
+	WINDOW_HEIGHT = SCREEN_HEIGHT * SCALE
 )
 
-type Game struct{}
+type GameState int
+
+const (
+	GameStateMenu = iota
+	GameStatePlaying
+)
+
+type Game struct {
+	bg *ebiten.Image
+
+	state GameState
+}
+
+func (g *Game) Init() error {
+	var err error
+	g.bg, _, err = ebitenutil.NewImageFromFile("assets/bg_320x256.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func (g *Game) Update() error {
+	switch g.state {
+	case GameStateMenu:
+		if inpututil.IsKeyJustPressed(ebiten.KeyZ) {
+			g.state = GameStatePlaying
+		}
+	case GameStatePlaying:
+		if inpututil.IsKeyJustPressed(ebiten.KeyZ) {
+			g.state = GameStateMenu
+		}
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(g.bg, nil)
+	ebitenutil.DebugPrint(screen, "Current state: "+strconv.Itoa(int(g.state)))
+}
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return SCREEN_WIDTH, SCREEN_HEIGHT
 }
 
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "This is a test.")
-}
-
 func main() {
-	ebiten.SetWindowSize(SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
-	ebiten.SetWindowTitle("Ebiten Test")
+	g := &Game{}
+	if err := g.Init(); err != nil {
+		log.Fatal(err)
+	}
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+	ebiten.SetWindowTitle("Gophy Runner")
+	ebiten.SetWindowResizable(false)
+
+	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
 	}
 }
