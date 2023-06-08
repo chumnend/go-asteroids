f01cf6bf3a39fae863014c32911d856add0bb732
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-06-08 18:57:39 -0400

    add comments to game.go

--- gunthur/game.go
+++ gunthur/game.go
@@ -11,20 +11,27 @@ const (
 	scale        = 2
 )
 
+// Game implements ebiten.Game interface
 type Game struct{}
 
+// NewVector returns a Vector struct, window width and window height
 func NewGame() (*Game, int, int) {
 	return &Game{}, screenWidth * 2, screenHeight * 2
 }
 
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
 	return nil
 }
 
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
