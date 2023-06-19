55af705fb3683a7b35cb0fddb094209dd1fb2d02
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-06-19 11:24:57 -0400

    bound player rectangle to screen

--- gunthur/sprite.go
+++ gunthur/sprite.go
@@ -2,8 +2,10 @@ package gunthur
 
 import (
 	"image/color"
+	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 // Sprite represents a game sprite
@@ -27,7 +29,18 @@ func NewSprite() *Sprite {
 // MoveTo changes the position of the sprite
 func (s *Sprite) MoveTo(x int, y int) {
 	s.x = x
+	if s.x < 0 {
+		s.x = 0
+	} else if x > screenWidth-s.width {
+		s.x = screenWidth - s.width
+	}
+
 	s.y = y
+	if s.y < 0 {
+		s.y = 0
+	} else if s.y > screenHeight-s.height {
+		s.y = screenHeight - s.height
+	}
 }
 
 func (s *Sprite) HandleInput(keys []ebiten.Key) {
@@ -51,6 +64,8 @@ func (s *Sprite) Update() error {
 
 // Draw takes an ebiten screen object and draws the sprite on it
 func (s *Sprite) Draw(screen *ebiten.Image, opts ebiten.DrawImageOptions) {
+	ebitenutil.DebugPrint(screen, "X: "+strconv.Itoa(s.x)+" Y:"+strconv.Itoa(s.y))
+
 	purple := color.RGBA{255, 0, 255, 255}
 	for x := s.x; x < s.x+s.width; x++ {
 		for y := s.y; y < s.y+s.height; y++ {
