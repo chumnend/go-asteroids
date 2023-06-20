b8982d19972b8323cff87ab2ed161beb821a1363
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-06-20 19:46:35 -0400

    rename sprite to player

--- gunthur/levels.go
+++ gunthur/levels.go
@@ -4,7 +4,7 @@ func loadLevels() map[int]*Scene {
 	levels := make(map[int]*Scene)
 
 	level1 := NewScene()
-	level1.AddComponent(NewSprite())
+	level1.AddComponent(NewPlayer())
 	levels[1] = level1
 
 	return levels
