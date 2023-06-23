612aa3ffbb3eaffdc7b1a3de83caac743bd4312f
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-06-23 14:03:29 -0700

    start from scratch

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"gophy-runner/gunthur"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
 func main() {
-	game, w, h := gunthur.NewGame()
-	ebiten.SetWindowSize(w, h)
-	ebiten.SetWindowTitle("Gunthur: The Game")
-	if err := ebiten.RunGame(game); err != nil {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
