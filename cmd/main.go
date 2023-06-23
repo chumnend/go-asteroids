a22f49c76baa82f8d9b8c4c51b660e2a58dee52f
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-06-23 14:14:33 -0700

    move screen size into constant variables

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+const (
+	// internal game size (before scaling)
+	gameScreenWidth  = 320
+	gameScreenHeight = 240
+
+	scale        = 2 // scale 200% in window
+	windowWidth  = gameScreenWidth * scale
+	windowHeight = gameScreenHeight * scale
+)
+
 type Game struct{}
 
 func (g *Game) Update() error {
@@ -17,13 +27,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return gameScreenWidth, gameScreenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowSize(windowWidth, windowHeight)
+	ebiten.SetWindowTitle("Gunthur: High-Speed Hack and Slash")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
