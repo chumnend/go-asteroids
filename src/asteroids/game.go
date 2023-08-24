07265b00d00506888c8e306f94a3ade3031b8359
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-08-24 19:02:21 -0400

    handle changing game states

--- src/asteroids/game.go
+++ src/asteroids/game.go
@@ -1,9 +1,13 @@
 package asteroids
 
 import (
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
@@ -15,8 +19,26 @@ const (
 	DT            = 1 / 60.0 // assume the delta is fixed and we are always at 60 FPS
 )
 
+type GameState int
+
+const (
+	GameStateMenu GameState = iota
+	GameStatePlaying
+)
+
+type MenuState int
+
+const (
+	MenuMain MenuState = iota
+	MenuPause
+	MenuGameOver
+)
+
 // Game implements the ebiten.Game interface
-type Game struct{}
+type Game struct {
+	gameState GameState
+	menuState MenuState
+}
 
 // NewGame returns a Game struct, the width of the window and the height of the window
 func NewGame() (*Game, int, int) {
@@ -52,9 +74,84 @@ func (game *Game) Draw(screen *ebiten.Image) {
 	if err := game.drawObjects(); err != nil {
 		log.Fatal(err)
 	}
+
+	// debug info
+	game.printDebugInfo(screen)
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 func (game *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return GAME_WIDTH, GAME_HEIGHT
+}
+
+// LOADING ========================================================================================
+
+// loadObjects loads all required assets for the game
+func (game *Game) loadObjects() error {
+	return nil
+}
+
+// HANDLE INPUT ===================================================================================
+
+// handleInput reads key inputs and performs actions
+func (game *Game) handleInput() error {
+	// force game end
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return ebiten.Termination
+	}
+
+	switch game.gameState {
+	case GameStateMenu:
+		switch game.menuState {
+		case MenuMain:
+			if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+				game.startGame()
+			}
+		case MenuPause:
+			if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+				game.resumeGame()
+			}
+		default:
+			return errors.New("unexpected menu state")
+		}
+	case GameStatePlaying:
+		if inpututil.IsKeyJustPressed(ebiten.KeyP) {
+			game.pauseGame()
+		}
+	default:
+		return errors.New("unexpected game state")
+	}
+
+	return nil
+}
+
+func (game *Game) startGame() {
+	game.gameState = GameStatePlaying
+}
+
+func (game *Game) pauseGame() {
+	game.gameState = GameStateMenu
+	game.menuState = MenuPause
+}
+
+func (game *Game) resumeGame() {
+	game.gameState = GameStatePlaying
+}
+
+// PROCESS GAME LOGIC =============================================================================
+
+// processLogic updates all game objects each frame
+func (game *Game) processLogic() error {
+	return nil
+}
+
+// PAINT SCREEN ===================================================================================
+
+// drawObjects redraws all objects on the screen
+func (game *Game) drawObjects() error {
+	return nil
+}
+
+func (game *Game) printDebugInfo(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("State: %v\nMenu: %v", game.gameState, game.menuState))
 }
